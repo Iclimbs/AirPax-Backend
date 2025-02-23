9e102893e000ecc9cf0743e51ca46ec01908384d
uttamiclimbs <uttamkrshaw@iclimbs.com>
2025-02-23 15:57:48 +0530

    Fixed Public Images Storage

--- controller/vehiclefeatures.js
+++ controller/vehiclefeatures.js
@@ -1,30 +1,19 @@
 require('dotenv').config()
 const express = require("express")
 const FeatureRouter = express.Router()
-const multer = require("multer");
-const path = require('node:path');
 const { vehicleFeaturesModel } = require('../model/vehiclefeatures.model');
+const { uploadMiddleWare } = require('../middleware/FileUpload');
 
-const uploadPath = path.join(__dirname, "../public/features");
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, uploadPath);
-    },
-    filename: function (req, file, cb) {
-        let uniqueSuffix = Date.now();
-        cb(null, uniqueSuffix + file.originalname);
-    },
-});
-
-const upload = multer({ storage: storage });
-
-
-
-FeatureRouter.post("/add", upload.single("img"), async (req, res) => {
+FeatureRouter.post("/add", uploadMiddleWare.single("img"), async (req, res) => {
     const { name } = req.body;
-    const fileName = req.file.filename;
+    if (!req.file) {
+        return res.json({
+            status: 'error',
+            message: 'Plase Upload Image'
+        })
+    }
     try {
-        const vehicle = new vehicleFeaturesModel({ name, img: fileName })
+        const vehicle = new vehicleFeaturesModel({ name, img: req?.file?.location })
         await vehicle.save()
         res.json({ status: "success", message: "New Vehicle Feature Added !!" })
     } catch (error) {
@@ -32,14 +21,14 @@ FeatureRouter.post("/add", upload.single("img"), async (req, res) => {
     }
 })
 
-FeatureRouter.patch("/edit/:id", upload.single("img"), async (req, res) => {
+FeatureRouter.patch("/edit/:id", uploadMiddleWare.single("img"), async (req, res) => {
     const { id } = req.params
-    const fileName = req.file.filename;
-
     try {
         const vehicle = await vehicleFeaturesModel.find({ _id: id })
         vehicle[0].name = req.body.name;
-        vehicle[0].img = fileName
+        if (req.file) {
+            vehicle[0].img = req?.file?.location
+        }
         await vehicle[0].save()
         res.json({ status: "success", message: "Features Details Successfully Updated !!" })
     } catch (error) {
@@ -53,9 +42,9 @@ FeatureRouter.patch("/disable/:id", async (req, res) => {
         const vehicle = await vehicleFeaturesModel.findById({ _id: id })
         vehicle.status = !vehicle.status;
         await vehicle.save()
-        res.json({ status: "success", message: "Feature Availability Updated !!" })
+        res.json({ status: "success", message: "Feature Status Updated !!" })
     } catch (error) {
-        res.json({ status: "error", message: `Failed To Update Feature Availability ${error.message}` })
+        res.json({ status: "error", message: `Failed To Update Vehicle Feature Status  ${error.message}` })
     }
 })
 
@@ -74,7 +63,7 @@ FeatureRouter.get("/listall", async (req, res) => {
 
 FeatureRouter.get("/listall/active", async (req, res) => {
     try {
-        const vehicleList = await vehicleFeaturesModel.find({ status: true },{CreatedAt:0,status:0})
+        const vehicleList = await vehicleFeaturesModel.find({ status: true }, { CreatedAt: 0, status: 0 })
         if (vehicleList.length !== 0) {
             res.json({ status: "success", data: vehicleList })
         } else {
