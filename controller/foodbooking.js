8c516f406d90e684c11cb1224864ed9dc43195e8
uttamiclimbs <uttamkrshaw@iclimbs.com>
2025-03-27 00:57:29 +0530

    Food Booking Work Successfull

--- controller/foodbooking.js
+++ controller/foodbooking.js
@@ -3,6 +3,7 @@ const express = require("express");
 const jwt = require('jsonwebtoken');
 const { FoodBookingModel } = require('../model/foodbooking.model');
 const { AdminAuthentication } = require('../middleware/Authorization');
+const { FoodAllocation } = require('../model/SupervisorReport.model');
 const FoodBookingRouter = express.Router()
 
 FoodBookingRouter.post("/add", AdminAuthentication, async (req, res) => {
@@ -13,9 +14,56 @@ FoodBookingRouter.post("/add", AdminAuthentication, async (req, res) => {
     try {
         const newfood = new FoodBookingModel({ foodItems, price, seatId, tripId, bookedBy: decoded._id })
         await newfood.save()
-        return res.json({ status: "success", message: "Food Order Successful !!" })
+
+        // Updating Allocated Food Details 
+        let foodServed = [];
+        let totalAllocatedFood;
+        // Get Food Items Which Are Served
+        for (let index = 0; index < foodItems.length; index++) {
+            foodServed.push(foodItems[index])
+        }
+
+        const allocatedFood = await FoodAllocation.find({ trip: tripId })
+        totalAllocatedFood = allocatedFood[0].foodUnit;
+
+        const servedMap = {};
+
+        foodServed.forEach(({ name, quantity }) => {
+            if (servedMap[name]) {
+                servedMap[name] += quantity;
+            } else {
+                servedMap[name] = quantity;
+            }
+        });
+
+        // Update quantities
+        totalAllocatedFood.forEach(foodItem => {
+            if (servedMap.hasOwnProperty(foodItem.foodName)) {
+                foodItem.quantity -= servedMap[foodItem.foodName];
+            }
+        });
+
+        const updateFoodAllocation = await FoodAllocation.findOneAndUpdate({ trip: tripId }, { $set: { foodUnit: totalAllocatedFood } }, { new: true });
+
+        if (updateFoodAllocation === null) {
+            return res.json({ status: 'error', message: `Failed To Update Food Allocation Details.` })
+        } else {
+            return res.json({ status: "success", message: "Food Order Successful !!" })
+        }
     } catch (error) {
         return res.json({ status: "error", message: `Failed To Order Food For The User ${error.message}` })
+    }
+})
+
+
+
+FoodBookingRouter.get("/detail/:id", AdminAuthentication, async (req, res) => {
+    const { id } = req.params;
+    try {
+        const foodOrdered = await FoodBookingModel.find({ tripId: id });
+        return res.json({ status: "success", data: foodOrdered });
+    } catch (error) {
+        return res.json({ status: "error", message: `Failed To Fet Food Ordered Details ${error.message}` })
     }
 })
 
