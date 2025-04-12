a9e2cb8d4035554783642510bc912065fc681ea3
uttamiclimbs <uttamkrshaw@iclimbs.com>
2025-04-12 15:30:49 +0530

    added bus no in pnr

--- controller/pnr.js
+++ controller/pnr.js
@@ -52,6 +52,7 @@ PnrRouter.get("/", async (req, res) => {
     details.journeystartdate = tripdetails[0].journeystartdate;
     details.journeyenddate = tripdetails[0].journeyenddate;
     details.busid = vehicledetails[0].gpsname;
+    details.busno = vehicledetails[0].name;
     details.starttime = tripdetails[0].starttime;
     details.endtime = tripdetails[0].endtime;
     details.totaltime = tripdetails[0].totaltime;
